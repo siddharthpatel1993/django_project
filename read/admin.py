e5b0cf895a2a7a8d3140b2792ae8fbb1d125184f
sidd <sidd@gmail.com>
2023-03-17 20:26:29 +0000

    version 5

--- read/admin.py
+++ read/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from write.models import MyModel
 
 # Register your models here.
+
+class MemberAdmin(admin.ModelAdmin):
+  list_display = ("fullname", "lastname", "age",)
+
+admin.site.register(MyModel, MemberAdmin)
