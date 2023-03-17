12b1712d3c3f49120dd5bdb603674e486b5dc234
sidd <sidd@gmail.com>
2023-03-17 18:39:25 +0000

    version 2

--- project/urls.py
+++ project/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('form/', include('write.urls'))
+    path('form/', include('write.urls')),
+    path('', include('read.urls')),
 ]
---
+++ read/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse
+from django.template import loader
+from write.models import MyModel
+
+def members(request):
+  mymembers = MyModel.objects.all().values()
+  template = loader.get_template('all_members.html')
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
+
+def details(request, id):
+  mymember = MyModel.objects.get(id=id)
+  template = loader.get_template('details.html')
+  context = {
+    'mymember': mymember,
+  }
+  return HttpResponse(template.render(context, request))
+
+def main(request):
+  template = loader.get_template('main.html')
+  return HttpResponse(template.render())
